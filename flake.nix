24bc338ccfbd6cdf3b265d65a9f7c1a251cf0732
6ix <101804748+Dymon1403@users.noreply.github.com>
2026-09-12 00:15:16 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "ThinkPad NixOS with Hyprland and Home Manager";
 
   inputs = {
-    # Основной репозиторий пакетов (unstable)
+    # Main pool pkgs (unstable)
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Home Manager
@@ -16,10 +16,10 @@
     nixosConfigurations.thinkpad = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        # Подключаем системный конфиг
+
         ./configuration.nix
 
-        # Подключаем home-manager как модуль
+        # Connect home-manager how module
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
